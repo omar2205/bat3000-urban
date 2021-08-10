6072c36020381701591ca7f5cf49338754dc70ca
omar2205 <omar2205a@yahoo.com>
2021-08-10 10:29:01 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'nl.oskr.bat3000',
+  appName: 'bat3000',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+}
+
+export default config
