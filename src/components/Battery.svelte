b8c42e9bb5b3457a2e5b0b9fefe9b85c8c32e5d2
omar2205 <omar2205a@yahoo.com>
2021-08-10 13:07:46 +0200

    fix float number

--- src/components/Battery.svelte
+++ src/components/Battery.svelte
@@ -5,7 +5,7 @@
   let isCharging = false
   const getBatteryInfo = async () => {
     let b = await Device.getBatteryInfo()
-    bat = (b.batteryLevel * 100) + '%'
+    bat = parseInt((b.batteryLevel * 100)) + '%'
     console.log(b)
     if (b.isCharging) isCharging = true
   }
@@ -14,13 +14,18 @@
   })
 </script>
 
-<h1>{bat ? bat : ''}</h1>
-<h3>{isCharging ? '⚡' : ''}</h3>
+<h1>{bat ? bat : ''}<h2>{isCharging ? '⚡' : ''}</h2></h1>
 
 <style lang="scss">
 h1 {
   font-size: 4rem;
   font-weight: 100;
   text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h2 {
+    font-size: 2rem;
+  }
 }
 </style>
