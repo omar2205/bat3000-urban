e8d7a955635a2043c4f28f97f159c0158bdfbf4e
omar2205 <omar2205a@yahoo.com>
2021-08-10 14:28:28 +0200

    style and fix battery

--- src/components/Battery.svelte
+++ src/components/Battery.svelte
@@ -2,28 +2,33 @@
   import { Device } from '@capacitor/device'
   import { onMount } from 'svelte'
   let bat
-  let isCharging = false
+  let isCharging
   const getBatteryInfo = async () => {
     let b = await Device.getBatteryInfo()
     bat = parseInt((b.batteryLevel * 100)) + '%'
-    console.log(b)
-    if (b.isCharging) isCharging = true
+    // console.log(b, isCharging)
+    isCharging = b.isCharging
   }
   onMount(() => {
-    getBatteryInfo()
+    setInterval(() => {
+      getBatteryInfo()
+    }, 1000)
   })
 </script>
 
-<h1>{bat ? bat : ''}<h2>{isCharging ? '⚡' : ''}</h2></h1>
+<h1>{bat ? bat : ''}<h2>{isCharging === true ? '⚡' : ''}</h2></h1>
 
 <style lang="scss">
 h1 {
-  font-size: 4rem;
+  font-size: 20vw;
   font-weight: 100;
   text-align: center;
   display: flex;
   justify-content: center;
   align-items: center;
+  height: calc(100vh - 65px);
+  margin: 0;
+  padding: 0;
   h2 {
     font-size: 2rem;
   }
