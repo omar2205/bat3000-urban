eb1515a64ba20c43d2d586964a745e55510670c2
omar2205 <omar2205a@yahoo.com>
2021-08-10 12:42:14 +0200

    add battery info -test

---
+++ src/components/Battery.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Device } from '@capacitor/device'
+  import { onMount } from 'svelte'
+  let bat
+  let isCharging = false
+  const getBatteryInfo = async () => {
+    let b = await Device.getBatteryInfo()
+    bat = (b.batteryLevel * 100) + '%'
+    console.log(b)
+    if (b.isCharging) isCharging = true
+  }
+  onMount(() => {
+    getBatteryInfo()
+  })
+</script>
+
+<h1>{bat ? bat : ''}</h1>
+<h3>{isCharging ? '⚡' : ''}</h3>
+
+<style lang="scss">
+h1 {
+  font-size: 4rem;
+  font-weight: 100;
+  text-align: center;
+}
+</style>
