928ba92b825ba5f482372a1257ffc6fe7d0d38ad
omar2205 <omar2205a@yahoo.com>
2021-08-21 19:43:37 +0200

    v1.3 re wrote logic

--- src/components/Battery.svelte
+++ src/components/Battery.svelte
@@ -24,83 +24,52 @@
   // if var bat changes, invoke checkBat()
   $: bat, checkBat()
 
+  const debug_send = async (msg) => {
+    http.post({
+      url: BACKEND_URL+'/h',
+      headers: { 'Content-Type': 'text/html; charset=utf-8' },
+      data: `---> ${msg}`
+    })
+  }
+
   const getBatteryInfo = async () => {
     let b = await Device.getBatteryInfo()
-    bat = parseInt((b.batteryLevel * 100)) + '%'
+    bat = Math.round(parseFloat(b.batteryLevel) * 100) + '%'
     // console.log(b, isCharging)
     isCharging = b.isCharging
   }
 
   onMount(() => {
     getBatteryInfo()
-    if (debug_mode === true) {
-      http.post({
-        url: BACKEND_URL+'/h',
-        headers: { 'Content-Type': 'text/html; charset=utf-8' },
-        data: '--->  Mounted'
-      })
-      http.post({
-        url: BACKEND_URL+'/h',
-        headers: { 'Content-Type': 'text/html; charset=utf-8' },
-        data: '--->  Check bg'
-      })
-      if (typeof cordova !== 'undefined') {
-        http.post({
-          url: BACKEND_URL+'/h',
-          headers: { 'Content-Type': 'text/html; charset=utf-8' },
-          data: '--->  ' + JSON.stringify(cordova.plugins.backgroundMode, null, 2)
-        })
-      }
+    if(debug_mode) debug_send('mounted')
+    if(debug_mode) debug_send(`current status - ${bat} ${isCharging ? 'Charging ⚡' : 'Not-Charging'}`)
 
-    }
-    if (typeof BackgroundMode !== 'undefined') {
-      // if BackgroundMode plugin is active and we
-      // didn't send an alert activate mode
-      if(sent_alert === false) BackgroundMode.enable(true)
-      else BackgroundMode.enable(false)
-    }
-    setInterval(() => {
-      getBatteryInfo()
-    }, 2000)
+    setInterval(getBatteryInfo, 2000)    
 
     App.addListener('appStateChange', ({ isActive }) => {
       state_isActive = isActive
-      if (debug_mode === true) {
-        http.post({
-          url: BACKEND_URL+'/h',
-          headers: { 'Content-Type': 'text/html; charset=utf-8' },
-          data: '--->  state change' + isActive + '///' + state_isActive
-        })
-      }
+      if(debug_mode) debug_send(`state-change ${isActive}`)
     })
   })
 
   const checkBat = async () => {
-    // if we already sent the request
-    if (sent_alert === true) return
+    if (sent_alert) return
     if (isCharging === false) return
-    if (parseInt(bat) >= CRITICAL_LVL) {
+    if(parseInt(bat) >= CRITICAL_LVL) {
       const res = await http.post({
-        url: BACKEND_URL_BAT,
+        url: BACKEND_URL,
         headers: { 'Content-Type': 'application/json' },
         data: { percentage: parseInt(bat), plugged: (isCharging ? 'PLUGGED' : 'UNPLUGGED') }
       })
       await Toast.show({
-        text: 'Battery charged. Sent req. Got: ' + await res.data.slice(0, 2),
+        text: 'Battery reached wanted charge level. sent request.',
         duration: 'long'
       })
-      // debug, send response
-      await http.post({
-        url: BACKEND_URL+'/h',
-        headers: { 'Content-Type': 'text/html; charset=utf-8' },
-        data: 'debug_data_post_res ' + JSON.stringify(await res, null, 2)
-      })
-      // we reached our desired battery level
-      // now we turn off background mode
       sent_alert = true
       if (typeof BackgroundMode !== 'undefined') BackgroundMode.enable(false)
     }
   }
+
 </script>
 
 <h1>{bat ? bat : ''}<h2>{isCharging === true ? '⚡' : ''}</h2>
